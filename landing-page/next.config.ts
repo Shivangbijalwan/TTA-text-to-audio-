eb7c866d773987e6bfd69398dba88c77f2c4c1a1
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-13 02:33:58 +0530

    force redeploy with correct root

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// force redeploy
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
