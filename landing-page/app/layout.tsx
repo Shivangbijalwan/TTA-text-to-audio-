34066f93787893fbd92f1f4b47f3546c03fe7810
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-14 23:07:32 +0530

    removed files which has clerk auth in it

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
 import {
   Geist,
   Geist_Mono,
@@ -52,9 +51,7 @@ export default function RootLayout({
       `}
     >
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
-          {children}
-        </ClerkProvider> 
+        {children}
       </body>
     </html>
   );
