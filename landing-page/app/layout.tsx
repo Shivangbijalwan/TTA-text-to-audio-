90ccbe7b30122520ec5bbbd54f7712c9dda8b435
Shivangbijalwan <shivangbijalwan771@gmail.com>
2026-05-15 15:07:00 +0530

    implimented vercel view analatics to the landing page

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Titan_One, Sour_Gummy } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
